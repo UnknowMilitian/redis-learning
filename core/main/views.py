34cd7a18bf0f72e4ec1464fb9be7c29bea77cadc
UnknowMilitiant <j.umariy4238@gmail.com>
2024-10-04 17:34:12 +0500

    Added cache

--- core/main/views.py
+++ core/main/views.py
@@ -10,15 +10,41 @@ from .serializers import CategorySerializer, ItemSerializer
 
 # Redis imports
 from django.views.decorators.cache import cache_page
+from django.core.cache import cache
 from django.utils.decorators import method_decorator
 
 
 # Create your views here.
 class CategoryAPIView(APIView):
     def get(self, request):
-        categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True)
+
+        categories = cache.get("categories")
+
+        if not categories:
+            categories = Category.objects.all()
+            serializer = CategorySerializer(categories, many=True)
+            cache.set("categories", serializer.data, timeout=60 * 5)
+        else:
+            serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            cache.delete("categories")
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, pk):
+        try:
+            category = Category.objects.get(pk=pk)
+            category.delete()
+
+            cache.delete("categories")
+            return Response(status=204)
+        except Category.DoesNotExist:
+            return Response(status=404)
 
 
 class ItemListAPIView(generics.ListAPIView):
